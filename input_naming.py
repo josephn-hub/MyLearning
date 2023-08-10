602bdd26814da6d8e54f6f0d1762def4c06c6589
josephkambham <josephkambham@gmail.com>
2023-08-11 00:20:57 +0100

    Added new file

---
+++ input_naming.py
@@ -0,0 +1,12 @@
+
+
+def isValid():
+    provideInput = input(' Enter whether the used is valid either True or False \n ')
+    if provideInput == 'True':
+        return True
+    else:
+        return False
+
+
+calling = isValid()
+print(calling)
